645798038dbb55933691493595627f1f51e7f4eb
Leticia Galego <leticiagalego12@gmail.com>
2026-09-23 21:41:34 -0300

    aula 1: leitura do CSV e estatísticas

--- aula01/app.py
+++ aula01/app.py
@@ -2,6 +2,23 @@
 """
 
 import streamlit as st
+import dados
 
+st.set_page_config(layout="wide")
 st.title("📚 Dashboard de Livros")
 st.write("Se você está vendo esta página, o seu ambiente está pronto! 🎉")
+
+col1, col2, col3 = st.columns(3)
+
+livros = dados.ler_livros(dados.CAMINHO_LIVROS)
+qtd_livros = len(livros)
+
+preco_medio = dados.calcular_preco_medio(livros)
+
+qtd_cinco_estrelas = dados.contar_cinco_estrelas(livros)
+
+col1.metric(label="Total de livros:", value=qtd_livros)
+col2.metric(label="Preco medio", value=f"£{preco_medio:.2f}")
+col3.metric (label="Livros com cinco estrelas", value=qtd_cinco_estrelas)
+
+st.dataframe(livros)
--- aula01/dados.py
+++ aula01/dados.py
@@ -2,8 +2,62 @@
 """
 
 from pathlib import Path
+import csv
 
 # Pasta onde este arquivo .py está. Assim o programa encontra o CSV
 # mesmo quando é executado a partir de outra pasta (como no Streamlit Cloud).
 PASTA = Path(__file__).parent
 CAMINHO_LIVROS = PASTA / "livros.csv"
+
+catalogo = []
+
+def calcular_preco_medio(livros):
+    if not livros:
+        return 0.0
+    soma: float = 0
+    for livro in livros:
+        preco_original: str = livro["preco"]
+        preco_original_limpo: str = preco_original.replace("£", "")
+        preco_num: float = float(preco_original_limpo)
+        soma += preco_num
+    preco_medio = soma / len(livros)
+    return preco_medio
+
+def contar_cinco_estrelas(livros):
+    contador = 0
+    for livro in livros:
+        if livro["nota"] == "Five":
+            contador += 1
+    return contador
+
+def ler_livros_v3(caminho):
+    with open(caminho, "r", encoding="utf-8") as arquivo:
+        leitor = csv.DictReader(arquivo)
+        for linha in leitor:
+            print(linha["titulo"])
+
+def ler_livros_v2(caminho):
+    with open(caminho, "r", encoding="utf-8") as arquivo:
+        print(arquivo.read())
+
+def ler_livros(caminho):
+    arquivo = None
+    livros = []
+    try:
+        arquivo = open(caminho, "r", encoding="utf-8")
+        leitor = csv.DictReader(arquivo)
+        for linha in leitor:
+            livros.append(linha)
+    except FileNotFoundError:
+        print("Ocorreu um erro na leitura do arquivo!")
+    except Exception as error:
+        print("Ocorreu um erro inesperado:", error)
+    finally:
+        if arquivo is not None:
+            arquivo.close()
+    return livros
+
+if __name__ == "__main__":
+    catalogo = ler_livros(CAMINHO_LIVROS)
+    print(f"A quantidade de livros eh {len(catalogo)}")
+    print(calcular_preco_medio(catalogo))
